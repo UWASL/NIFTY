428efc8fe4511db776151fc9ff410bacf00cb07a
salkiswany <salkiswany@gmail.com>
2020-06-16 15:06:25 -0400

    Update partitioner.cpp

--- src/partitioner.cpp
+++ src/partitioner.cpp
@@ -1,15 +1,25 @@
+/**
+* TODO
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 string MY_MAC = "";
 
+/**
+* TODO
+*/
 void installRule(string rule)
 {
 	printf("Installing rule: %s\n", rule.c_str());
 	system(rule.c_str());
 }
 
+/**
+* TODO
+*/
 void print_usage()
 {
 	printf("::USAGE::\n");
@@ -24,17 +34,22 @@ void print_usage()
 	exit(1);
 }
 
-// Creates a MAC-based PNP where g1 cannot reach g2. (everyone else can reach both)
-// Uses the "cookie" value of 10 (0xa) for the installed OpenFlow rules. This helps with healing the partition as we only remove rules with 0xa. 
-// Make sure no other rules in the OpenFlow table use the cookie 0xa as well.
+/**
+* Creates a MAC-based PNP where g1 cannot reach g2. (everyone else can reach both)
+* Uses the "cookie" value of 10 (0xa) for the installed OpenFlow rules. 
+* This helps with healing the partition as we only remove rules with 0xa. 
+* Make sure no other rules in the OpenFlow table use the cookie 0xa as well.
+*/
 void createMACPNP(const vector<string> g1, const vector<string> g2)
 {
-	if(find(g1.begin(), g1.end(), MY_MAC)!=g1.end()) // I'm in g1, shouldn't reach any g2 members
+	if(find(g1.begin(), g1.end(), MY_MAC)!=g1.end()) {// I'm in g1, shouldn't reach any g2 members
 		for (int i = 0; i < g2.size(); ++i)
-			installRule("sudo ovs-ofctl add-flow br0 cookie=10,priority=10000,dl_src="+g2[i]+",action=drop");
-	else if(find(g2.begin(), g2.end(), MY_MAC)!=g2.end())
+			installRule("sudo ovs-ofctl add-flow br0 cookie=10,priority=10000,dl_src="+g2[i]+",action=drop"); //TODO remove sudo as we discussed
+	}
+	else if(find(g2.begin(), g2.end(), MY_MAC)!=g2.end()) {
 		for (int i = 0; i < g1.size(); ++i)
 			installRule("sudo ovs-ofctl add-flow br0 cookie=10,priority=10000,dl_src="+g1[i]+",action=drop");
+	}
 	//Do nothing otherwise (I'm part of g3)
 }
 
@@ -80,14 +95,15 @@ int main(int argc, char** argv)
 		g2.push_back(temp);
 	}
 
-	printf("Creating a PNP between the following g1 & g2.\n");
-	printf("Members of g1: \n");
+	printf("Creating a partial partition between the following group 1 and group 2.\n");
+	printf("Members of group 1: \n");
 	for (int i = 0; i < g1.size(); ++i)
 		printf("%s\n", g1[i].c_str());
 	printf("\n~~~~~~~~~~~~~~~\n\n");
-	printf("Members of g2: \n");
+	printf("Members of group 2: \n");
 	for (int i = 0; i < g2.size(); ++i)
 		printf("%s\n", g2[i].c_str());
+	
 	createMACPNP(g1,g2);
 	fin.close();
 
