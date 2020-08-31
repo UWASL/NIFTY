e068fdd0984cd6aca08d6978b2e82193b03bebab
maaalfat <maaalfat@node3.maaalfat-qv79174.sds-pg0.utah.cloudlab.us>
2020-08-31 01:01:33 -0600

    fixed windows stupid format

--- deploy/nifty/deploy.sh
+++ deploy/nifty/deploy.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # get NIFTY_HOME
-source ../../configuration
+. ../../configuration
 
 # If your ssh need more options, you can set them in the $sshOptions variable (you can set identify file, port, ...)
-sshOptions=" -n -o StrictHostKeyChecking=no -i /proj/sds-PG0/mohammed/pnpdaemon/scripts/id_rsa -p 22"
-
+sshOptions=" -T -o StrictHostKeyChecking=no -i /proj/sds-PG0/mohammed/pnpdaemon/scripts/id_rsa "
 
 # Iterate through all the nodes, get their brdige IPs and MACs and save them (used to update nifty's nodes.conf)
 ips="";
 macs="";
+nodesCount=0;
 while IFS= read -r nodeIP
 do
 	if [ -z $nodeIP ]; then
 		continue;
 	fi
   	#ssh into the node and get its IP and MAC.
-  	ip=$(ssh $sshOptions $nodeIP /sbin/ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
-  	mac=$(ssh $sshOptions $nodeIP cat /sys/class/net/br0/address)
+  	ip=$(ssh -n $sshOptions $nodeIP /sbin/ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+  	mac=$(ssh -n $sshOptions $nodeIP cat /sys/class/net/br0/address)
 
 	ips="${ips}${ip}\n"
 	macs="${macs}${mac}\n"
+	let nodesCount=nodesCount+1
 done < nodes.conf
 
-confFileContents=$(printf "%d\n%b%b" $num $ips $macs)
+printf "%d\n%b%b" $nodesCount $ips $macs > temp.conf
 # For each of the nodes in deployment, update nodes.conf & run nifty with the nodes IP.
 while IFS= read -r nodeIP
 do
@@ -31,8 +32,12 @@ do
 		continue;
 	fi
   	#ssh into the node and get its IP and MAC.
-  	ip=$(ssh $sshOptions $nodeIP /sbin/ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
-  	mac=$(ssh $sshOptions $nodeIP cat /sys/class/net/br0/address)
-
-  	ssh $sshOptions $nodeIP "echo $confFileContents > $NIFTY_HOME/nodes.conf"
+  	ip=$(ssh -n $sshOptions $nodeIP /sbin/ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+  	mac=$(ssh -n $sshOptions $nodeIP cat /sys/class/net/br0/address)
+	scp $sshOptions ./temp.conf $nodeIP:"${NIFTY_HOME}/nodes.conf"
+	
+	echo "Starting NIFTY on node $nodeIP (which has IP address: $ip, and MAC address: $mac)"
+	ssh $sshOptions $nodeIP "sudo $NIFTY_HOME/nifty -t 1 -i $ip -m $mac" &
+ 
 done < nodes.conf
+rm ./temp.conf
