a979d07634d684fd8a1c9a5555476ce00c971b43
salkiswany <salkiswany@gmail.com>
2020-09-01 23:16:00 -0400

    Update deploy_nifty.sh

--- deploy/deploy_nifty.sh
+++ deploy/deploy_nifty.sh
@@ -24,7 +24,7 @@ do
 	let nodesCount=nodesCount+1
 done < ./nodes.conf
 
-printf "%d\n%b%b" $nodesCount $ips $macs > temp.conf
+printf "%d\n%b%b" $nodesCount $ips $macs > nifty_nodes.conf
 # For each of the nodes in deployment, update nodes.conf & run nifty with the nodes IP.
 while IFS= read -r nodeIP
 do
@@ -34,12 +34,12 @@ do
   	#ssh into the node and get its IP and MAC.
   	ip=$(ssh -n $sshOptions $nodeIP /sbin/ifconfig br0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
   	mac=$(ssh -n $sshOptions $nodeIP cat /sys/class/net/br0/address)
-	scp $sshOptions ./temp.conf $nodeIP:"${NIFTY_HOME}/nodes.conf"
+	scp $sshOptions ./nifty_nodes.conf $nodeIP:"${NIFTY_HOME}/nifty_nodes.conf"
 	
 	echo "Starting NIFTY on node $nodeIP (which has IP address: $ip, and MAC address: $mac)"
 	# Could need to either run the script as sudo or add sudo here to be able to deploy rules. (or have OVS not require sudo)
-	ssh -n $sshOptions $nodeIP "cd $NIFTY_HOME && ./nifty -t 1 -i $ip -m $mac" &
+	ssh -n $sshOptions $nodeIP "cd $NIFTY_HOME && ./nifty -t 1 -i $ip -m $mac nifty_nodes.conf" &
  
 done < ./nodes.conf
 
-rm ./temp.conf
+rm ./nifty_nodes.conf
